e7531c9becf1c0aa42d5f8cabb12baa4af796707
SuganSelvam <sugan170@gmail.com>
2020-08-07 10:43:12 +0530

    Intial Upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Post from "./Components/Post/Post"
+import Left from "./Left"
+import Right from "./Right"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Left className="item"></Left>
+      <Post className="item"></Post>
+      <Right className="item"></Right>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
